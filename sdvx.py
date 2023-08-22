693f05ad45e59eae410f1872dff0bfe27b9a27dd
Izuna127 <izuna@izuna.net>
2023-08-22 10:06:01 +0900

    add comments

--- sdvx.py
+++ sdvx.py
@@ -14,6 +14,7 @@ client = discord.Client(intents = intents)
 tree = app_commands.CommandTree(client)
 
 
+# 3分ごとにゲームをプレイとして楽曲情報を更新する
 @client.event
 async def on_ready():
     await tree.sync()
@@ -28,6 +29,7 @@ async def on_ready():
         await asyncio.sleep(180)
 
 
+# スラッシュコマンド: sdvx
 @tree.command(name = "sdvx", description = "おうちボルテ曲ガチャだよ")
 @app_commands.rename(level = "レベル")
 @app_commands.describe(level = "レベルを1～20の間で指定します")
@@ -38,16 +40,17 @@ async def on_ready():
 async def sdvx(interaction: discord.Interaction, level: int, diff: str = None, cond: str = None):
     cnx = connectToDB()
     target = select(cnx, level, difficulty = diff, condition = cond)
+    
+    if cnx is not None and cnx.is_connected():
+        cnx.close()
 
     emoji = discord.utils.get(client.emojis, name = target[2].lower())
     embed = discord.Embed(title = "おうちボルテガチャ", description = f"【{target[0]} {str(emoji)} Lv.{target[3]}】をプレイしなさい！", color = 0xee17ca)
     embed.set_footer(text = f"プレイ条件は【{target[4]}】よ") 
     await interaction.response.send_message(embed = embed)
 
-    if cnx is not None and cnx.is_connected():
-        cnx.close()
 
-
+# データベースに接続するやつ
 def connectToDB():
     try:
         cnx = mysql.connector.connect(
@@ -57,12 +60,13 @@ def connectToDB():
                 port = os.getenv('DB_PORT'),
                 database = os.getenv('DB_NAME')
         )
-
         return cnx
+
     except Exception as e:
         print(f"Error occurred: {e}")
 
 
+# データベースから楽曲情報を取得するやつ
 def select(cnx, level, difficulty = None, condition = None):
     sql = "select * from HouseSDVX where level = %s"
     param = (level,)
@@ -81,7 +85,6 @@ def select(cnx, level, difficulty = None, condition = None):
 
     return target
 
-#    return f"{target[0]}:{target[2]}: Lv.{target[3]}\nプレイ条件は【{target[4]}】よ"
 
 if __name__ == '__main__':
     client.run(os.getenv('DISCORD_TOKEN'))
